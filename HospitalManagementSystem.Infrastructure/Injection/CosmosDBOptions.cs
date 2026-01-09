c1dc9ed68178dc12ea014a8cef52e80ba78ea576
Admin <Admin@SOFTSUAVE>
2026-01-09 11:01:22 +0530

    After DI

---
+++ HospitalManagementSystem.Infrastructure/Injection/CosmosDBOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagementSystem.Infrastructure.Injection
+{
+    public class CosmosDbOptions
+    {
+        public string AccountEndpoint { get; set; } = null!;
+        public string AccountKey { get; set; } = null!;
+    }
+}
--- HospitalManagementSystem.Infrastructure/Injection/DependencyInjection.cs
+++ HospitalManagementSystem.Infrastructure/Injection/DependencyInjection.cs
@@ -1,32 +1,73 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿//using Microsoft.Azure.Cosmos;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+
+//namespace HospitalManagementSystem.Infrastructure.Injection;
+
+//public static class DependencyInjection
+//{
+//    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+//    {
+//        services.AddSingleton<CosmosClient>(_ =>
+//        {
+//            var endpoint = configuration["CosmosDb:AccountEndpoint"];
+//            var key = configuration["CosmosDb:AccountKey"];
+
+//            var options = new CosmosClientOptions
+//            {
+//                HttpClientFactory = () =>
+//                {
+//                    var handler = new HttpClientHandler
+//                    {
+//                        ServerCertificateCustomValidationCallback =
+//                            (req, cert, chain, errors) => true
+//                    };
+//                    return new HttpClient(handler);
+//                }
+//            };
+
+//            return new CosmosClient(endpoint, key, options);
+//        });
+
+//        return services;
+//    }
+//}
+
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace HospitalManagementSystem.Infrastructure.Injection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddSingleton<CosmosClient>(_ =>
+        // Bind CosmosDbOptions
+        services.AddOptions<CosmosDbOptions>()
+            .Bind(configuration.GetSection("CosmosDb"))
+            .Validate(o =>
+                !string.IsNullOrWhiteSpace(o.AccountEndpoint) &&
+                !string.IsNullOrWhiteSpace(o.AccountKey),
+                "CosmosDb configuration is invalid")
+            .ValidateOnStart();
+
+        // Register CosmosClient
+        services.AddSingleton<CosmosClient>(sp =>
         {
-            var endpoint = configuration["CosmosDb:AccountEndpoint"];
-            var key = configuration["CosmosDb:AccountKey"];
+            var options = sp
+                .GetRequiredService<IOptions<CosmosDbOptions>>()
+                .Value;
 
-            var options = new CosmosClientOptions
-            {
-                HttpClientFactory = () =>
-                {
-                    var handler = new HttpClientHandler
-                    {
-                        ServerCertificateCustomValidationCallback =
-                            (req, cert, chain, errors) => true
-                    };
-                    return new HttpClient(handler);
-                }
-            };
+            var clientOptions = new CosmosClientOptions();
 
-            return new CosmosClient(endpoint, key, options);
+            return new CosmosClient(
+                options.AccountEndpoint,
+                options.AccountKey,
+                clientOptions);
         });
 
         return services;
